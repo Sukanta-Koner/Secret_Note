03dcc3ec55c334beaabc93fa22a7a7de5d058739
Sukanta Koner <sukantakoner005@gmail.com>
2020-05-26 04:52:05 +0530

    Added docstring for functions + fer optimisation in the code

--- Secret_Note.py
+++ Secret_Note.py
@@ -5,8 +5,8 @@ from os import mkdir
 from os import remove
 import shutil
 
-db_path = './DataBase/'
-password_file = db_path+"passwordInfo.txt"
+db_path = './SNDataBase/'
+password_file = db_path+"SNPasswordInfo.txt"
 
 menu ='''
     1. Register to Secret Note
@@ -14,7 +14,7 @@ menu ='''
     3. Retrieve Password
     4. Exit
     What do you want to do: '''
-    
+
 menu1 = '''
 What do you want to do :
     1. Keep a secret
@@ -31,8 +31,31 @@ secret exists with same title.
     Press other key to Cancel.
 '''
 
-def check_in_file(filename, username, password='', mode=''):
-    with open(filename, 'rt') as fd:
+def check_in_file(username, password='', mode=''):
+    """check-in-file - This function read and username and password from SNPasswordInfo file.
+    depending on mode valuer it will do the following tasks:
+    mode-------------task
+    user             It will check if the user is existing user or not.
+    password         It will check if password for that perticular user is correct or not.
+    retrieve         It will print the password for that given user name
+
+
+    Arguments:
+        username {str} -- User provided username, which will get validated in this function.
+
+    Keyword Arguments:
+        password {str} -- User provided password for validation while login (default: {''})
+        mode     {str} -- Depending on mode this function will perform below operations (default: {''})
+                            mode-------------task
+                            user             It will check if the user is existing user or not.
+                            password         It will check if password for that perticular user is correct or not.
+                            retrieve         It will print the password for that given user name
+
+    Returns:
+        {bool}  -- In success cases return True
+                -- In failure cases return False
+    """
+    with open(password_file, 'rt') as fd:
         for line in fd:
             line = line.split('\n')[0].split(',')
             if mode == 'password' and line == [username,password]:
@@ -46,24 +69,59 @@ def check_in_file(filename, username, password='', mode=''):
             return False
 
 def password_check(username):
+    """password_check - This function will take password as input from user while login and send both username and password for validation.
+
+    Arguments:
+        username {str} -- user provided username.
+
+    Returns:
+        {bool} --   True - While login if username-password pair is correct.
+                    False- If username-password pair is wrong.
+    """
     password = input("Password: ")
-    check_status = check_in_file(password_file, username, password, mode='password')
+    check_status = check_in_file(username, password, mode='password')
     return check_status
 
 def create_dic(user):
+    """create_dic - This function will create a directory for a user in side database folder.
+
+    Arguments:
+        user {str} -- This will be the name of the directory.
+
+    Returns:
+        {bool}  --  True  - If directory created successfully
+                    False - If failed to create the directory
+    """
     user = user.strip()
     if user != "":
-        dir_path = './DataBase/'+user
+        dir_path = db_path+user
         mkdir(dir_path)
         return True
     else:
         return False
 
 def fileWrite(data, filename, mode):
+    """fileWrite - It will write the data into the file named 'filename'
+
+    Arguments:
+        data     {str} -- data to be written in to the file
+        filename {str} -- where the data will be written
+        mode     {str} --   'w' - overwrite the old data and re-write the new data
+                            'a' - append the new data after the old data.
+    """
     with open(filename, mode) as fd:
         fd.write(data+'\n')
 
 def new_password(username):
+    """new_password - This will ask user to create password while registering as new user. and send both user and password to store.
+
+    Arguments:
+        username {str} -- User given username while registering as new user.
+
+    Returns:
+        [bool]  --  True - If password is created successfully
+                    False - Failed to create proper password.
+    """
     Password = input('Create New Password : ')
     try_count = 3
     while Password == "" and try_count > 0:
@@ -72,23 +130,45 @@ def new_password(username):
     if try_count == 0 and Password == "":
         return False
     else:
-        data = username+','+Password+'\n'
+        data = username+','+Password
         fileWrite(data, password_file, 'a')
         return True
 
 def create_user(user, user_list=[]):
+    """create_user - This will validate and ask user to create new username while registering as new user if 1st username input is invalid.
+
+    Arguments:
+        user {str} -- While registering, 1st username given by user
+
+    Keyword Arguments:
+        user_list {list} -- list of usernames already available (default: {[]})
+
+    Returns:
+        user {str} - new generated username
+    """
     user = user.strip()
     if user_list:
         if (not user) or (user in user_list):
             user = input("username already used. Try another username: ").strip().upper()
-            user, _ = create_user(user, user_list)
+            user = create_user(user, user_list)
     else:
         if not user :
             user = input("invalid. Try another username: ").strip().upper()
-            user, _ = create_user(user, user_list)
-    return (user,True)
+            user = create_user(user, user_list)
+    return user
 
 def select_user_or_create():
+    """select_user_or_create - This function is responsible for taking the 1st input from user from the following options:
+        -register
+        -login
+        -retrieve password
+        -exit
+        depending on the user input this will perform that operation.
+
+    Returns:
+        (verified_user,user) {tuple} -- verified_user - flag to check user authentication
+                                        user - username given by user.
+    """
     verified_user = False
     user = ''
     login_check = input(menu)
@@ -100,21 +180,20 @@ def select_user_or_create():
             mkdir(db_path)
             user_list = [d for d in listdir(db_path) if path.isdir(path.join(db_path, d))]
         user = input("Enter username: ").strip().upper()
-        user, flag = create_user(user, user_list)
-        if flag:
-            verified_user = new_password(user)
-            if not verified_user:
-                    print('Invalid password')
-                    verified_user, user = select_user_or_create()
-            else:
-                print(f'''
+        user = create_user(user, user_list)
+        verified_user = new_password(user)
+        if not verified_user:
+                print('Invalid password')
+                verified_user, user = select_user_or_create()
+        else:
+            print(f'''
         ==Welcome {user} to Secret Note==\n
         ''')
     elif login_check == '2':
         print("\n====Login===")
         user = input("Enter username: ").strip().upper()
         if path.exists(password_file) and user != "":
-            if check_in_file(password_file, user, mode = 'user'):
+            if check_in_file(user, mode = 'user'):
                 verified_user = password_check(user)
                 if not verified_user:
                     print('Invalid password')
@@ -133,7 +212,7 @@ def select_user_or_create():
         print("\n====Retrieve password===")
         user = input("Enter username: ").strip().upper()
         if path.exists(password_file) and user != "":
-            if not check_in_file(password_file, user, mode='retrieve'):
+            if not check_in_file(user, mode='retrieve'):
                 print('username not found\n')
         else:
             print("Username not found\n")
@@ -149,6 +228,11 @@ def select_user_or_create():
     return (verified_user,user)
 
 def keep_secrets(user):
+    """keep_secrets - This will take input from user about the 'Title' and the 'Note' for a Secret. If given title is same as any old title user has to choose whether to overwrite/append/cancel the operation. after one successfull secret write user will deside whether to keep another secter or exit.
+
+    Arguments:
+        user {str} -- logedin user
+    """
     while(True):
         filename = input('Title : ').strip()
         if filename == "":
@@ -184,6 +268,11 @@ def keep_secrets(user):
             break
 
 def show_sectets(user):
+    """show_sectets - This will show user all the sectet titles. and prompt user if user want to view any perticular secret.
+
+    Arguments:
+        user {str} -- logedin username
+    """
     file_list = [d for d in listdir(db_path + user)]
     if len(file_list) != 0:
         print('=================Your secrets===================')
@@ -206,6 +295,11 @@ def show_sectets(user):
         print("No secrets found")
 
 def delete_secret(user):
+    """delete_secret - This will prompt all the secrets that perticulat user have and ask user to input the sectet name which user want to delete.
+
+    Arguments:
+        user {str} -- username of the logged profile.
+    """
     file_list = [d for d in listdir(db_path + user)]
     if len(file_list) != 0:
         print('=================Your secrets===================')
@@ -225,6 +319,15 @@ def delete_secret(user):
         print("No secrets found")
 
 def delete_profile(user):
+    """delete_profile - This function will delete the profile for that perticular user, including password registry.
+
+    Arguments:
+        user {str} -- username of the profile to be deleted.
+
+    Returns:
+        {bool} --   True  - If profile got deleted successfully
+                    False - If profile is not deleted.
+    """
     profile_flag = input('Do you really want to delete the profile(No):').lower()
     if profile_flag in y_flags:
         shutil.rmtree(db_path + user)
@@ -241,6 +344,8 @@ def delete_profile(user):
     return False
 
 def run():
+    """run - This is the starting point.
+    """
     verified_user, username = select_user_or_create()
     if verified_user:
         try:
